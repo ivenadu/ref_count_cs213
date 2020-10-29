d2fc0be7ba96dc8bc91d556268b01399f0c0f764
ivenadu <ivena2012@gmail.com>
2020-10-29 00:08:25 -0700

    quiz 5 problem 3

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+void* rc_malloc (int nbytes) {
+    int * ref_count = malloc (nbytes + 8);
+    *ref_count = 1;
+    return ((void*) ref_count) + 8;
+}
+
+int* w = NULL;
+int* x = NULL;
+int* y = NULL;
+int* z = NULL;
+
+void foo() {
+ int* a = rc_malloc(100);
+    x = a;
+    y = a;
+    z = x;
+    w = x;   
+}
+
+int main(){
+    foo();
+    printf("%d ", *(int*)((void*)y - 8) );
+}
